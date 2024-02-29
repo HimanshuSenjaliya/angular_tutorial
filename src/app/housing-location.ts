14a4eef16310ad59bfd977bb02a12bbc265f56b9
HimanshuSenjaliya <himanshusenjaliya11@gmail.com>
2024-02-29 12:53:14 +0530

    first commit

---
+++ src/app/housing-location.ts
@@ -0,0 +1,10 @@
+export interface HousingLocation {
+  id: number;
+  city: string;
+  name: string;
+  state: string;
+  photo: string;
+  availableUnits: number;
+  wifi: boolean;
+  laundry: boolean;
+}
